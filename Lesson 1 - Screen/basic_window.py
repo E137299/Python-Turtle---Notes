107641437fd9bbf3f5f7a38fe82a832af1bc4b88
David Yin <29613693+E137299@users.noreply.github.com>
2024-02-20 16:01:45 -0600

    Lesson 1 - Screen

---
+++ Lesson 1 - Screen/basic_window.py
@@ -0,0 +1,13 @@
+'''
+THIS CODE CAN BE USED AS STARTER CODE FOR ANY TURTLE PROGRAM THAT YOU EVER WANT TO WRITE. 
+'''
+
+
+# Provides the program access to all the classes and functions with in the Turtle library
+from turtle import *
+
+# Creates a Screen object
+display = Screen()
+
+# mainloop() keeps the screen open until it is manually closed
+display.mainloop()
---
+++ Lesson 1 - Screen/window_attributes.py
@@ -0,0 +1,21 @@
+'''
+The following code demonstrates how methods belonging to the Screen class 
+can be used to customize the display window.
+'''
+
+# Provides the program access to all the classes and functions with in the Turtle library
+from turtle import *
+
+# Creates a Screen object
+display = Screen()
+display.title("Customized Window") #.title(title) places a title at the top of the window
+display.bgcolor("teal") #.bgcolor(color) set the color of the display
+display.setup(1200, 600) #.setup(width, height) defines the dimensions of the window. 
+display.tracer(0) #.tracer(n) Controls the animation speed. n specifies the number of turtle moves before the screen updates. Passing n=0 will update the screen after each turtle move.
+
+
+
+
+
+# mainloop() keeps the screen open until it is manually closed
+display.mainloop()
