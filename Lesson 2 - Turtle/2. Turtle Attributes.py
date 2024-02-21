5ca4a9c5a2ea48a5cf89dd7b2ccf9541738b0e4f
David Yin <29613693+E137299@users.noreply.github.com>
2024-02-21 10:02:55 -0600

    Getters and Setters

--- Lesson 2 - Turtle/2. Turtle Attributes.py
+++ Lesson 2 - Turtle/2. Turtle Attributes.py
@@ -1,13 +1,15 @@
 from turtle import *
+from random import randint, uniform, choice
 
 display = Screen()
-
-# Instantiating a Turtle
-first = Turtle()
+display.bgcolor("black")
 
 '''
 SETTING A TURTLE'S ATTRIBUTES
 '''
+# Instantiating a Turtle
+first = Turtle()
+
 # Shape - turtle.shape(shape) sets the turtle's shape. Options: turtle, circle, square, triangle, arrow
 first.shape("turtle") 
 
@@ -19,5 +21,30 @@ first.color("teal")
 first.speed(0)
 
 # Pen Up
+first.penup()
+
+# Set heading
+first.setheading(45)
+
+
+
+'''
+CHECK A TURTLE'S ATTRIBUTES
+'''
+second = Turtle()
+second.color(uniform(0.5,1.0), uniform(0.5,1.0), uniform(0.5,1.0))
+second.shape(choice(["circle","square","triangle","turtle","arrow"]))
+second.setheading(randint(0,360))
+second.pencolor(uniform(0.5,1.0), uniform(0.5,1.0), uniform(0.5,1.0))
+second.setposition(randint(-150,150),randint(-150,150))
+
+print("The second turtle's color is "+str(second.color()[1]))
+print("The second turtle's pen color is "+str(second.pencolor()))
+print("The second turtle's shape is "+second.shape())
+print("The second turtle's heading is "+str(second.heading()))
+print("The second turtle's position is "+str(second.position()))
+
+
 
 display.mainloop()
+
--- Lesson 2 - Turtle/3. Turtle Methods.py
+++ Lesson 2 - Turtle/3. Turtle Methods.py
@@ -1,1 +1,39 @@
-# https://docs.python.org/3/library/turtle.html#turtle-methods
+from turtle import *
+
+display = Screen()
+display.bgcolor("teal")
+
+# Instantiating a Turtle
+first = Turtle()
+first.shape("square")
+first.color("red")
+
+second = Turtle()
+second.shape("circle")
+second.color("yellow")
+
+'''
+Movement using forward(), left(), right()
+'''
+for side in range(4):
+    for steps in range(5):
+        first.pendown()
+        first.forward(10)
+        first.penup()
+        first.forward(10)
+    first.right(90)
+
+'''
+Movement by assigning position to turtle .goto(x,y) or setposition(x,y)
+'''
+second.penup()
+second.goto(150,150)
+second.pendown()
+second.pensize(10)
+second.goto(150,-150)
+second.goto(-150,-150)
+second.goto(-150,150)
+second.goto(150,150)
+
+display.mainloop()
+
