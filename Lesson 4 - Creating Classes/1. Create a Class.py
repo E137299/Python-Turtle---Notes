8ec90de8823e0e2c970ea8298d781667be59626f
David Yin <29613693+E137299@users.noreply.github.com>
2024-02-21 16:23:30 -0600

    Notes Near Complete

--- Lesson 4 - Creating Classes/1. Create a Class.py
+++ Lesson 4 - Creating Classes/1. Create a Class.py
@@ -1,36 +1,79 @@
 from turtle import *
 
+def draw_boundary():
+    t = Turtle()
+    t.speed(0)
+    t.hideturtle()
+    t.pencolor("yellow")
+    t.penup()
+    t.goto(-300,300)
+    t.pensize(10)
+    t.pendown()
+    for side in range(4):
+        t.forward(600)
+        t.right(90)
+
 class Player(Turtle):
-    def __init__(self, x, y, color, screen, left_key, right_key):
+    def __init__(self, x, y, shape, screen, left_key, right_key):
         super().__init__()
         self.speed(0)
         self.penup()
         self.hideturtle()
         self.goto(x,y)
-        self.color(color)
-        self.shape("turtle")
+        self.colors = [(0.0,.7,0.0), (0.0,.8,0.0), (0.0,.9,0.0),(0.0,1.0,0.0)]
+        self.sick_index = 0
+        self.color(self.colors[self.sick_index])
+        self.shape(shape)
         self.showturtle()
-        self.health = 5
-        screen.onkeypress(self.turn_left, left_key)
-        screen.onkeypress(self.turn_right, right_key)
+        self.screen = screen
+        self.screen.onkeypress(self.turn_left, left_key)
+        self.screen.onkeypress(self.turn_right, right_key)
+    
+    def move(self):
+        if self.xcor()>290 or self.xcor()<-290:
+            self.sick_index += 1
+            self.get_sicker()
+            self.setheading(180-self.heading())
+        if self.ycor()>290 or self.ycor()<-290:
+            self.sick_index += 1
+            self.get_sicker()
+            self.color(self.colors[self.sick_index])
+            self.setheading(-1*self.heading())
+        self.forward(2)
 
     def turn_right(self):
-        print("right")
         self.right(10)
 
     def turn_left(self):
         self.left(10)
 
-    def sick(self, screen):
-        screen.bgcolor("red")
-        self.color("green")
+    def get_sicker(self):
+        self.color(self.colors[self.sick_index])
+
+    def die(self, screen):
+        self.get_sicker()
+        self.sick_index=0
 
 display = Screen()
-display.bgcolor("teal")
+display.title("Fade to Green")
+display.bgcolor((0.0,1.0,0.0))
+display.listen()
 
-player = Player(100, 100, "yellow", display, "Left", "Right")
+draw_boundary()
+
+one = Player(-100,0, "circle", display, "a","b")
+two = Player(100, 0, "square", display, "Left", "Right")
+
 
 while True:
-    player.forward(2)
+    one.move()
+    two.move()
+    if one.distance(two)<20:
+        one.sick_index += 1
+        two.sick_index += 1
+    if one.sick_index == 3:
+        one.die(display)
+    if two.sick_index == 3:
+        two.die(display)
 
 display.mainloop()
